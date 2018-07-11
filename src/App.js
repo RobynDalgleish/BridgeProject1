585d8416c4bedc381156cba99b31437d8cbc7faa
RobynDalgleish <h0arifyed@gmail.com>
2018-07-11 17:00:27 -0400

    got latitude and logitude

--- src/App.js
+++ src/App.js
@@ -20,22 +20,22 @@ class App extends Component {
   }
 
   // 1.) onChange, setState to the value of the input fields. Store the product and location in state.
-  initialQuerySearch = (e) => {
+  initialQuerySearch = e => {
     this.setState({productQuery: e.target.value});
   }
-  initialLocationSearch = (e) => {
+  initialLocationSearch = e => {
     this.setState({locationQuery: e.target.value});
   }
 
   // 2.) onSubmit, prevent the page from reloading, and call the getProductId function passing in 'inputedProduct' as a parameter (productQuery from state)
-  submit = (e) => {
+  submit = e => {
     e.preventDefault();
     const inputedProduct = this.state.productQuery;
     this.getProductId(inputedProduct);
   }
 
   // 3.) Using the inputProduct, get make a call to the LCBO API to and store the resulting data in state. Render the names of everything in the productsArray as buttons.
-  getProductId = (inputedProduct) => {
+  getProductId = inputedProduct => {
     console.log(inputedProduct);
     fetchLcboEndpoint("products", {
         q: `${inputedProduct}`
@@ -48,11 +48,11 @@ class App extends Component {
   }
 
   // 4.) onClick (of a button containing the name of a product) filter the productsArray to grab the id of the corresponding product name, Then call the getLocationsForGmap function.
-    click = (e) => {
+    click = e => {
       e.preventDefault();
       console.log(e.target.id)
       const chosenProduct = e.target.id;
-      const productsArrayFiltered = this.state.productsArray.filter((result) => result.name === chosenProduct)
+      const productsArrayFiltered = this.state.productsArray.filter(result => result.name === chosenProduct)
       console.log(productsArrayFiltered)
       const productId = productsArrayFiltered[0].id
       console.log(productId)
@@ -62,21 +62,27 @@ class App extends Component {
 
   // 5.) Use the product id and the location as query parameters to grab the stores that fit both those criteria (have the product and are in that location). Store that as storeLocationsArray in state.
   getStoreLocations = (productId, inputedLocation) => {
-      fetchLcboEndpoint("stores", {
-          product_id: `${productId}`,
-          geo: `${inputedLocation}`,
-      }).then(data => {
-        console.log(data);
-        this.setState({
-          storeLocationsArray: data.result
-        })
-      });
+    fetchLcboEndpoint("stores", {
+        product_id: `${productId}`,
+        geo: `${inputedLocation}`,
+    }).then(data => {
+      console.log(data);
+      this.setState({
+        storeLocationsArray: data.result
+      })
       this.getLatLng();
+    });
   }
 
   // 6.) Map through the storeLocationsArray to grab the latitude and longitude of each unique store.
   getLatLng = () => {
-
+    const latLngArray = this.state.storeLocationsArray.map(result => {
+      // console.log(result.latitude, result.longitude);
+      return (
+        [result.latitude, result.longitude]
+      )
+    })
+    console.log(latLngArray);
   }
   // MyMapComponent = (props) =>
   //   <GoogleMap
@@ -90,17 +96,23 @@ class App extends Component {
   render() {
     return (
       <div>
+
         <form onSubmit={this.submit}>
-          <label htmlFor="productQuery"><span className="visuallyhidden">Search for an LCBO product</span></label>
+
+          <label htmlFor="productQuery">
+            <span className="visuallyhidden">Search for an LCBO product</span>
+          </label>
           <input type="text" id="productQuery" value={this.state.productQuery} onChange={this.initialQuerySearch} placeholder="Search for an LCBO product" />
 
-          <label htmlFor="locationQuery"><span className="visuallyhidden">Enter a location</span></label>
+          <label htmlFor="locationQuery">
+            <span className="visuallyhidden">Enter a location</span>
+          </label>
           <input type="text" id="locationQuery" value={this.state.locationQuery} onChange={this.initialLocationSearch} placeholder="Enter a location" />
-          <button 
-            type="submit"> 
-            Go! 
-          </button>
+
+          <button type="submit"> Go! </button>
+
         </form>
+
         <div>
           {
             this.state.productsArray.map((result, i) => {
@@ -112,6 +124,7 @@ class App extends Component {
             })
           }
         </div>
+
       </div>
     );
   }
